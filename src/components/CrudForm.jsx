453b06bdc30a3cac115678c9c46180b34e2594d6
Alberto Mario Guerrero G <psico.amgg@gmail.com>
2023-08-14 23:37:36 -0500

    finished

--- src/components/CrudForm.jsx
+++ src/components/CrudForm.jsx
@@ -47,7 +47,7 @@ const CrudForm = ({ createData, updateData, dataToEdit, setDataToEdit }) => {
 
   return (
     <div>
-      <h3>AGREGAR</h3>
+      <h3>{dataToEdit ? "Editar" : "Agregar"}</h3>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
