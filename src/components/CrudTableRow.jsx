aaea6c0ced13379ae0b61e9908d7b2d139392bf3
Alberto Mario Guerrero G <psico.amgg@gmail.com>
2023-08-15 14:17:11 -0500

    finished

--- src/components/CrudTableRow.jsx
+++ src/components/CrudTableRow.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
+import deleteIcon from "../assets/eliminar.png";
+import editIcon from "../assets/editar.png";
 
 const CrudTableRow = ({ rowData, setDataToEdit, deleteData }) => {
   let { name, diagnostico, id } = rowData;
@@ -11,8 +13,13 @@ const CrudTableRow = ({ rowData, setDataToEdit, deleteData }) => {
       <td> {diagnostico} </td>
       <td>
         {" "}
-        <button onClick={() => setDataToEdit(rowData)}>Editar </button>
-        <button onClick={() => deleteData(id)}>Eliminar </button>{" "}
+        <button onClick={() => setDataToEdit(rowData)}>
+          <img src={editIcon} alt="Icono" height={30} />{" "}
+        </button>
+        <button onClick={() => deleteData(id)}>
+          {" "}
+          <img src={deleteIcon} alt="Icono" height={30} />{" "}
+        </button>{" "}
       </td>
     </tr>
   );
